a0932e47cef22656563cd302e1d6beb2808df5ba
Nicolas Vargas Ortega <nvortega92@uma.es>
2015-05-11 17:34:56 +0200

    Aumentar intentos y poner fecha cuando falla

--- procedures/PKG_CORRECCION_EJERCICIOSBody.sql
+++ procedures/PKG_CORRECCION_EJERCICIOSBody.sql
@@ -3,55 +3,53 @@ create or replace PACKAGE BODY PKG_CORRECCION_EJERCICIOS AS
   --Un procedimiento que corrige un solo ejercicio de un solo alumno
   --param: dni alumno, id ejercicio, respuesta del alumno
   PROCEDURE PR_CORREC_EJER_ALUMNO(DNI IN Respuesta.dni_alumno%TYPE, ID_EJER IN Ejercicio.id_ejercicio%TYPE, ANSWER IN Respuesta.respuesta%TYPE) AS
+    
     -- Una variable para guardar la solucion ideal del ejercicio
     -- La solucion es un script de sentencias ejecutables
     VAR_EJERCICIO EJERCICIO.SOLUCION%TYPE;
     VAR_NOTA_EJER EJERCICIO.PUNTOS%TYPE;
     -- Otra variable para almacenar el numero de diferentes filas entre la solucion del alumno y la ideal
     VAR_CONT NUMBER;
+    
+    
   BEGIN
-
-    SELECT Respuesta.NOTA INTO VAR_NOTA_EJER FROM Respuesta WHERE Respuesta.id_ejercicio=ID_EJERCICIO AND Respuesta.dni_alumno=DNI;
-    IF VAR_NOTA_EJER = NULL THEN
-      SELECT Ejercicio.solucion INTO VAR_EJERCICIO FROM Ejercicio WHERE Ejercicio.id_ejercicio = ID_EJER;
-      -- Creamos una vista para comparar las salidas del script del alumno y el de la solucion ideal 
-      --DBMS_OUTPUT.PUT_LINE('CREATE OR REPLACE VIEW V$CORRECCION AS (('|| VAR_EJERCICIO || ' MINUS ' || ANSWER || ') UNION (' ||ANSWER|| ' MINUS ' || VAR_EJERCICIO || '))');
-      EXECUTE IMMEDIATE 'create or replace VIEW V$CORRECCION AS (('|| VAR_EJERCICIO || ' MINUS ' || ANSWER || ') UNION (' ||ANSWER|| ' MINUS ' || VAR_EJERCICIO || '))' ;
-      -- Si las salidas son iguales el numero de filas de la vista debe ser 0 
-      SELECT COUNT(*) INTO VAR_CONT FROM V$CORRECCION ;
-      -- Siempre aumentamos en 1 el numero de intentos
-      UPDATE Respuesta SET INTENTOS = INTENTOS+1 WHERE Respuesta.id_ejercicio = ID_EJERCICIO AND Respuesta.dni_alumno = DNI;
-      -- Al tener la respuesta correcta, se almacena la fecha de entrega
-      IF VAR_CONT = 0 THEN
-        SELECT EJERCICIO.PUNTOS INTO VAR_NOTA_EJER FROM EJERCICIO WHERE EJERCICIO.ID_EJERCICIO = ID_EJER;
-        UPDATE Respuesta SET SUBMITTED_AT = SYSDATE WHERE Respuesta.id_ejercicio = ID_EJERCICIO AND Respuesta.dni_alumno = DNI;
-        UPDATE Respuesta SET NOTA = VAR_NOTA_EJER WHERE Respuesta.id_ejercicio = ID_EJERCICIO AND Respuesta.dni_alumno = DNI;
-        DBMS_OUTPUT.PUT_LINE('RESPUESTA CORRECTA');
-      ELSE
-        DBMS_OUTPUT.PUT_LINE('RESPUESTA INCORRECTA'); -- Feedback
-      -- Creo que hay que checker antes si submitted at es nulo para poder actualizar cnd entregue la respuesta correcta en el caso contrario
-      END IF;
-
+  
+  
+    VAR_NOTA_EJER := 0; -- Iniciamos la nota a 0 por si no consigue tener bien la respuesta.
+    
+    -- Guardamos la solcuion del ejercicio en VAR_EJERCICIO
     SELECT Ejercicio.solucion INTO VAR_EJERCICIO FROM Ejercicio WHERE Ejercicio.id_ejercicio = ID_EJER;
-    -- Creamos una vista para comparar las salidas del script del alumno y el de la solucion ideal 
-    --DBMS_OUTPUT.PUT_LINE('CREATE OR REPLACE VIEW V$CORRECCION AS (('|| VAR_EJERCICIO || ' MINUS ' || ANSWER || ') UNION (' ||ANSWER|| ' MINUS ' || VAR_EJERCICIO || '))');
+    
+    
+    -- Corregimos el ejercicio haciendo un minus de la consulta correcta y la del alumno. 
     EXECUTE IMMEDIATE 'SELECT COUNT(*) FROM (('|| VAR_EJERCICIO || ' MINUS ' || ANSWER || ') UNION (' ||ANSWER|| ' MINUS ' || VAR_EJERCICIO || '))' INTO VAR_CONT;
+    
+    
     -- Siempre aumentamos en 1 el numero de intentos
     UPDATE Respuesta SET INTENTOS = INTENTOS+1 WHERE Respuesta.id_ejercicio = ID_EJER AND Respuesta.dni_alumno = DNI;
+    
+    
     -- Al tener la respuesta correcta, se almacena la fecha de entrega
     IF VAR_CONT = 0 THEN
       SELECT EJERCICIO.PUNTOS INTO VAR_NOTA_EJER FROM EJERCICIO WHERE EJERCICIO.ID_EJERCICIO = ID_EJER;
       UPDATE Respuesta SET SUBMITTED_AT = SYSDATE WHERE Respuesta.id_ejercicio = ID_EJER AND Respuesta.dni_alumno = DNI;
       UPDATE Respuesta SET NOTA = VAR_NOTA_EJER WHERE Respuesta.id_ejercicio = ID_EJER AND Respuesta.dni_alumno = DNI;
       DBMS_OUTPUT.PUT_LINE('RESPUESTA CORRECTA');
+    -- Si tiene la respuesta incorrecta tambien se almacena la fecha y la nota será cero.
     ELSE
-      DBMS_OUTPUT.PUT_LINE('RESPUESTA INCORRECTA'); -- Feedback
+      DBMS_OUTPUT.PUT_LINE('RESPUESTA INCORRECTA');
+      UPDATE Respuesta SET SUBMITTED_AT = SYSDATE WHERE Respuesta.id_ejercicio = ID_EJER AND Respuesta.dni_alumno = DNI;
+      UPDATE Respuesta SET NOTA = VAR_NOTA_EJER WHERE Respuesta.id_ejercicio = ID_EJER AND Respuesta.dni_alumno = DNI;
+      -- Feedback
     -- Creo que hay que checker antes si submitted at es nulo para poder actualizar cnd entregue la respuesta correcta en el caso contrario
->>>>>>> 03603ad28aa2d39f76a494b73f663a5f74371ceb
     END IF;
     
+    
     EXCEPTION
-      WHEN OTHERS THEN
+      WHEN OTHERS THEN -- Si tiene algun error tambien se almacena la fecha, la nota y los intentos
+        UPDATE Respuesta SET INTENTOS = INTENTOS+1 WHERE Respuesta.id_ejercicio = ID_EJER AND Respuesta.dni_alumno = DNI;
+        UPDATE Respuesta SET SUBMITTED_AT = SYSDATE WHERE Respuesta.id_ejercicio = ID_EJER AND Respuesta.dni_alumno = DNI;
+        UPDATE Respuesta SET NOTA = VAR_NOTA_EJER WHERE Respuesta.id_ejercicio = ID_EJER AND Respuesta.dni_alumno = DNI;
         IF SQLCODE = -1789 THEN
           DBMS_OUTPUT.PUT_LINE('ERROR: Las columnas de la respuesta y la solucion del ejercicio no coinciden.');
         ELSIF SQLCODE = -942 THEN
@@ -59,7 +57,13 @@ create or replace PACKAGE BODY PKG_CORRECCION_EJERCICIOS AS
         ELSE
           DBMS_OUTPUT.PUT_LINE('ERROR: ' || SQLERRM);
         END IF;
+        
+        
   END PR_CORREC_EJER_ALUMNO;
+  
+  ------------------------------                                                 ------------------------------
+  ------------------------------ Correccion de todos los ejercicios de un alumno ------------------------------
+  ------------------------------                                                 ------------------------------
   
   -- Un procedimiento que corrige todos los ejercicios de un solo alumno
   PROCEDURE PR_CORREC_ALL_EJER_ALUMNO (DNI IN Respuesta.dni_alumno%TYPE) AS
@@ -70,6 +74,13 @@ create or replace PACKAGE BODY PKG_CORRECCION_EJERCICIOS AS
       PR_CORREC_EJER_ALUMNO(DNI,VAR_RESPUESTA.id_ejercicio,VAR_RESPUESTA.respuesta);
     END LOOP;
   END PR_CORREC_ALL_EJER_ALUMNO;
+  
+  
+  ------------------------------                                                         ------------------------------
+  ------------------------------ Correccion de todos los ejercicios de todos los alumnos ------------------------------
+  ------------------------------                                                         ------------------------------
+  
+  
   
   -- Un procedimiento que todos los ejercicios de todos los alumnos de una asignatura 
   PROCEDURE PR_CORREC_ALL_ALUMNOS (ASIGNATURA_COD Asignatura.codigo%TYPE) AS
