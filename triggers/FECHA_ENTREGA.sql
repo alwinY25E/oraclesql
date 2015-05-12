da8f8ab66db20a1d8f7d6804dbf7219098d413eb
Adrián <adrigonle@gmail.com>
2015-05-12 16:46:05 +0200

    Update FECHA_ENTREGA.sql
    Terminado el trigger que controla la fecha de entrega

--- triggers/FECHA_ENTREGA.sql
+++ triggers/FECHA_ENTREGA.sql
@@ -1,27 +1,25 @@
-create or replace TRIGGER FECHA_ENTREGA 
--- Antes y para cada uno de los insert que hagamos (row)
+create or replace TRIGGER FECHA_ENTREGA
+-- Antes del insert para todas las filas
 BEFORE INSERT ON RESPUESTA FOR EACH ROW
 DECLARE
   -- Variable para guardar la fecha de entrega
   VAR_ENTREGA Relacion.fecha_entrega%TYPE;
 BEGIN
-  -- Seleccionamos la fecha de entrega que corresponde al ejercicio que
-  -- tratamos de insertar (no es seguro que este bien, pero en general funciona)
-  select distinct rel.fecha_entrega into VAR_ENTREGA from respuesta r 
-  join relacion_matricula rm on 
+  -- Seleccionamos la fecha de entrega que corresponde a cada ejercicio
+  -- para el cual estamos insertando una repsuesta
+  SELECT DISTINCT rel.fecha_entrega INTO VAR_ENTREGA FROM respuesta r 
+  JOIN relacion_matricula rm ON 
     ( r.dni_alumno = rm.matricula_dni_alumno AND
       r.codigo = rm.matricula_codigo AND
       r.curso_academico = rm.matricula_curso_academico )
-  join relacion rel on rm.numero_de_relacion = rel.id_relacion
-  where r.dni_alumno = :new.dni_alumno AND
+  JOIN relacion rel ON rm.numero_de_relacion = rel.id_relacion
+  WHERE r.dni_alumno = :new.dni_alumno AND
   r.codigo = :new.codigo AND
   r.curso_academico = :new.curso_academico;
   
-  -- Comprobamos qe este bien o no la fecha de entrega
-  IF :new.submitted_at <= VAR_ENTREGA THEN
-    DBMS_OUTPUT.PUT_LINE('Bien entregado');
-  ELSE
-    DBMS_OUTPUT.PUT_LINE('Te pasaste');
+  -- Si la fecha es mayor que la entrega, rechazamos el insert y lanzamos error
+  IF :new.submitted_at > VAR_ENTREGA THEN
+    raise_application_error(-20101, 'La fecha de entrega ya ha pasado.');
   END IF;
   
 END;
